527ebdc404f77a46ce78293967d2dca5d2579a08
rmlopes <rmlopes@dei.uc.pt>
2012-11-12 21:58:57 +0000

    N-bit parity problem for cellcode (stream-like inputs).

---
+++ examples/cellparity.py
@@ -0,0 +1,50 @@
+import sys
+from code.evodevo import *
+from code.operators import *
+from code.cellcode import *
+from code.utils.mathlogic import *
+from code.extendedarn import bindparams, displayARNresults
+
+def test(phen, rangelist ):
+    ok = 0
+    for i in rangelist:
+        r = evaluate(phen,i)
+        if r == 0:
+            ok += 1
+    return len(rangelist) - ok
+
+def evaluate(phenotype, nbits = 3, **kwargs):
+        mapfun = getbinaryoutput
+        try:
+                mapfun = kwargs['mapfun']
+        except KeyError: pass
+
+        ok = 0
+        for cur in range(pow(2,nbits)):
+                t = BitStream(uint=cur,length=nbits)
+                for i in t.bin:
+                        #print i
+                        norm = [int(i)*0.1]
+                        phenotype.nstepsim(kwargs['simtime'],*norm)
+                        #print 'OUT: ', out
+                out = mapfun(phenotype, **kwargs)
+                if int(out) == int(t.count(1)%2):
+                        ok += 1
+
+        return  pow(2,nbits) - ok
+
+if __name__ == '__main__':
+    p = CellProb(evaluate, 1, 1)
+    edw = EvoDevoWorkbench(sys.argv[1],p,Cell)
+    p.eval_ = bindparams(edw.arnconfig, p.eval_)
+    edw.run()
+
+    f = open('genome.save','w')
+    f.write(edw.best.phenotype.code.bin)
+    f.close
+
+    #plot_ = bindparams(edw.arnconfig, plotindividual)
+    #plot_(edw.best.phenotype)
+    genresult = test(ewd.best.phenotype, range(2,20))
+    print 'Generalization: '
+    print genresult
