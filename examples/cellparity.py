545246069e6a9fbfee3ffb1071b90343b1464d18
rmlopes <rmlopes@dei.uc.pt>
2012-11-22 12:41:51 +0000

    alternative fitness: step by step incrementing (fixed) train stream

--- examples/cellparity.py
+++ examples/cellparity.py
@@ -13,6 +13,8 @@ def test(phen, rangelist ):
             ok += 1
     return len(rangelist) - ok
 
+trainstream = '0101100111001100010111010111000010110100100010101'
+
 def evaluate(phenotype, nbits = 3, **kwargs):
         mapfun = getbinaryoutput
         try:
@@ -20,18 +22,21 @@ def evaluate(phenotype, nbits = 3, **kwargs):
         except KeyError: pass
 
         ok = 0
-        for cur in range(pow(2,nbits)):
-                t = BitStream(uint=cur,length=nbits)
-                for i in t.bin:
-                        #print i
-                        norm = [int(i)*0.1]
-                        phenotype.nstepsim(kwargs['simtime'],*norm)
-                        #print 'OUT: ', out
-                out = mapfun(phenotype, **kwargs)
-                if int(out) == int(t.count(1)%2):
-                        ok += 1
+        #for cur in range(pow(2,nbits)):
+        #t = BitStream(uint=cur,length=nbits)
+        t = BitStream(bin=trainstream)
+        for i in range(len(t.bin)):
+            #print i
+            norm = [int(t.bin[i])*0.1]
+            phenotype.nstepsim(kwargs['simtime'],*norm)
+            #print 'OUT: ', out
+            out = mapfun(phenotype, **kwargs)
+            if bool(out) == bool(not (t[:i+1].count(1)%2)):
+                ok += 1
+            #print '%s: %i (%i)' % (t[:i+1].bin, out, ok)
 
-        return  pow(2,nbits) - ok
+        #return  pow(2,nbits) - ok
+        return len(trainstream) - ok
 
 if __name__ == '__main__':
     p = CellProb(evaluate, 1, 1)
@@ -45,6 +50,6 @@ if __name__ == '__main__':
 
     #plot_ = bindparams(edw.arnconfig, plotindividual)
     #plot_(edw.best.phenotype)
-    genresult = test(ewd.best.phenotype, range(2,20))
+    genresult = test(edw.best.phenotype, range(2,20))
     print 'Generalization: '
     print genresult
