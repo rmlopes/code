51772936d55a49f02942ce7f191d23e9cb060c6d
rmlopes <rmlopes@dei.uc.pt>
2012-12-03 18:32:20 +0000

    Added uniform xover

--- code/operators.py
+++ code/operators.py
@@ -40,6 +40,23 @@ def onepoint_xover(code1,code2,*args):
         o2 = BitStream(bin=(code2[:p]+code1[p:]).bin)
         return o1, o2
 
+def uniform_xover(code1, code2, *args):
+    l = min(len(code1),len(code2))
+    mask = reduce(lambda x,y: "%s%s"%(x,y),
+                 [int(round(random.random()))
+                  for i in range(l)])
+    bitmask = BitStream(bin=mask)
+    print bitmask.bin
+    o1,o2 = '',''
+    for i in range(len(bitmask.bin)):
+        if bitmask[i]:
+            o1 += code1.bin[i]
+            o2 += code2.bin[i]
+        else:
+            o1 += code2.bin[i]
+            o2 += code1.bin[i]
+    return (BitStream(bin = o1), BitStream(bin = o2))
+
 if __name__ == '__main__':
     a = BitStream('0b' + '1'*20)
     print a.bin
@@ -55,3 +72,9 @@ if __name__ == '__main__':
     c,d = twopoint_xover(a,b)
     print c.bin
     print d.bin
+
+    a = BitStream('0b' + '1'*10)
+    b = BitStream('0b' + '0'*20)
+    e,f = uniform_xover(a,b)
+    print e.bin
+    print f.bin
