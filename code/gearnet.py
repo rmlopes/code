9b1afaed5cb522b587a9225de62d9e66fe797dbe
rmlopes <rmlopes@dei.uc.pt>
2013-02-01 19:20:47 +0000

    GEARNet baseline.

---
+++ code/gearnet.py
@@ -0,0 +1,173 @@
+import logging
+from functools import partial
+from bitstring import BitStream
+import math
+from arn import *
+import rencode
+from evodevo import Problem, Agent
+from utils import *
+from utils.bitstrutils import *
+import copy
+
+log = logging.getLogger(__name__)
+
+def printrencode2(phenotype, **kwargs):
+    return rencode.printdotcircuit(
+            phenotype.circuits[phenotype.output_idx],**kwargs)
+
+
+def printmultiplecircuit(phenotype, labels=None, arnet = None):
+    circuit = phenotype.getcircuit(phenotype.output_idx)
+    if not arnet:
+        arnet = phenotype.arnet
+    s = 'digraph best {\nordering = out;\n'
+    for c in circuit:
+        shape='oval'
+        if c[0] in arnet.effectorproms:
+            shape='hexagon'
+        elif c[0] in arnet.receptorproms:
+            shape='rectangle'
+        s += '%i [label="%s",shape="%s"];\n' % (c[0], c[1], shape)
+                #else labels[c[1]])
+        for inp in c[2]:
+            aux = "dir=back"
+            if inp < 0:
+                aux += ",style=dotted"
+            s += '%i -> %i [%s];\n' % (c[0],abs(inp),aux)
+    s += '}'
+    return s
+
+### Problem base to use with ReNCoDe
+class Prob():
+        def __init__(self, evaluate, grammar, startcodon, **kwargs):
+            try:
+                pp = kwargs['printf']
+            except KeyError:
+                self.print_ = (lambda x: x.circuit)
+            self.eval_ = evaluate
+            self.grammar = grammar
+            self.start = startcodon
+
+
+### Agent model to use with this CoDe module
+class GEARNetAgent(Agent):
+        genotype = None
+        phenotype = None
+        fitness = None
+        def __init__(self, config, problem, gcode = None, parentfit = 1e4):
+                Agent.__init__(self, parentfit)
+                generator = bindparams(config, self.generate)
+                if gcode == None:
+                    numtfs = 0
+                    while not numtfs:
+                        arnet = ARNetwork(generator(),config, problem=problem)
+                        numtfs = arnet.numtf
+                else:
+                    arnet = ARNetwork(gcode,config, problem=problem)
+                self.genotype = arnet
+                self.genotype.simulate()
+                #print arnet.promlist
+                self.phenotype = Phenotype(arnet,problem)
+                self.problem = problem
+                self.fitness = 1e9
+
+        def __str__(self):
+                return "### Agent ###\n%s\n%s: %f" % (self.arn,self.phenotype,
+                                                      self.fitness)
+
+        def pickled(self):
+            return self.genotype.code.bin
+
+class DMAgent(GEARNetAgent):
+        def __init__(self, config, problem, gcode = None, parentfit = 1e4):
+                self.generate = generatechromo
+                GEARNetAgent.__init__(self, config, problem, gcode, parentfit)
+
+class RndAgent(GEARNetAgent):
+        def __init__(self, config, problem, gcode = None, parentfit = 1e4):
+            self.generate = partial(generatechromo_rnd,
+                    genomesize = 32 * pow(2,config.getint('default','initdm')))
+            GEARNetAgent.__init__(self, config, problem, gcode, parentfit)
+
+def replace( intnum, key, g):
+    i = intnum % len(g[key])
+    log.debug("Returning rule " + str(i) + " for key " +
+              key + " -> "+str( g[key][i]))
+    return g[key][i]
+
+class Phenotype:
+    def __init__(self, arnet, problem):
+        self.problem = problem
+        self.arnet = arnet
+        self.int_sequence = self.extract_int_seq()
+        #print self.int_sequence
+        self.circuit = reduce(lambda l,m: l+m,
+                              self.translate(copy.deepcopy(self.int_sequence),
+                                             problem.grammar,problem.start))
+        #print self.circuit
+
+    def extract_int_seq(self):
+        sz = 8
+        functions = [[p[0],
+                      [p[4][i*sz:sz+i*sz].uint for i in range(len(p[4])/sz)],
+                      #[p[4][i*sz:sz+i*sz].count(1) for i in range(len(p[4])/sz)],
+                      p[5]] for p in self.arnet.proteins]
+        functions.sort(key=lambda function: function[2],reverse=True)
+        log.debug(functions)
+        return [el for f in functions for el in f[1]]
+
+    def translate(self, int_sequence, grm, tree ,originalseq=[], wrap=0):
+        if len(tree) == 0:
+            return []
+        #print "top el: ", tree[0]
+        #print grm.iterkeys()
+        if tree[0] in grm.iterkeys():
+            if len(int_sequence) == 0:
+                if wrap:
+                    int_sequence.extend(originalseq)
+                    wrap -= 1
+                else:
+                    return tree
+            x = int_sequence.pop(0)
+            #print tree
+            tree = replace(x,tree[0],grm) + tree[1:]
+            return self.translate(int_sequence, grm,
+                                  tree,originalseq,wrap)
+        else:
+            return [tree[0]] + self.translate(int_sequence, grm,
+                                            tree[1:],originalseq,wrap)
+
+
+
+###########################################################################
+### Test                                                                ###
+###########################################################################
+
+if __name__ == '__main__':
+        import ConfigParser
+        import random
+        from bitstring import BitStream
+        from bitstringutils import dm_event
+        from arn import ARNetwork
+        from evodevo import Problem
+
+        log.setLevel(logging.DEBUG)
+        cfg = ConfigParser.ConfigParser()
+        cfg.readfp(open('test-rencode.cfg'))
+        arncfg = ConfigParser.ConfigParser()
+        arncfg.readfp(open('arnsim-rencode2.cfg'))
+        proteins=[]
+        nump = 0
+        while nump < 3:
+                genome = BitStream(float=random.random(), length=32)
+                for i in range(5):
+                        genome = dm_event(genome,
+                                          .02)
+
+                arnet = ARNetwork(genome, arncfg)
+                nump = len(arnet.promlist)
+        for p in arnet.proteins: print p
+        prob = Problem(defaultnodemap,regressionfun, None)
+        circuit = buildcircuit(arnet, prob,False)
+        _printcircuit(circuit)
+        print printdotcircuit(circuit, prob.labels)
---
+++ examples/gearnet-symb.py
@@ -0,0 +1,62 @@
+from code.operators import *
+from code.utils import *
+from code.utils.mathlogic import *
+from code.evodevo import EvoDevoWorkbench
+from code.gearnet import *
+from code.grammars import grammardb
+import logging
+import sys
+
+
+log = logging.getLogger(__name__)
+
+def drange(start, stop, step):
+    r = start
+    while r < stop:
+        yield r
+        r += step
+
+def kozapolynomial(inp):
+    return pow(inp,2) - 2*pow(inp,4) + pow(inp,6)
+
+def quarticpolynomial(inp):
+    return pow(inp,4) + pow(inp,3) + pow(inp,2) + inp
+
+def evaluate(phenotype, target, inputs, grm = grammardb._base_grammar):
+    circuit = phenotype.circuit
+    #print 'duh'
+    #print circuit
+    for k in grm.iterkeys():
+        #print 'key:', k
+        if k != 'inp' and circuit.find(k) != -1:
+            return 1e9
+
+    errors = [abs(target(inp) -
+                  eval(circuit))
+              for inp in inputs]
+    try:
+        sum_ = sum(errors)
+    except:
+        log.warning('Invalid individual: overflow error...')
+        return 100
+
+    return 100 if math.isinf(sum_) else sum_
+
+
+if __name__ == '__main__':
+    log.setLevel(logging.DEBUG)
+    evalfun = partial(evaluate,
+                      target=kozapolynomial,
+                      inputs=list(drange(-1,1.1,.1)),
+                      grm = grammardb._fixed_grammar)
+    p = Prob(evalfun, grammardb._fixed_grammar, ['expr'])
+
+    try:
+        agentclass = getattr(__import__('__main__'),sys.argv[2])
+    except:
+        print 'Loading default agent class'
+        agentclass = DMAgent
+
+    edw = EvoDevoWorkbench(sys.argv[1],p,agentclass)
+    edw.run()
+    print edw.best.phenotype.circuit
