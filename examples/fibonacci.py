7a30096b9960f47583095448579072351942c3c3
Rui Lopes <rui.lourenco.lopes@gmail.com>
2013-12-12 17:33:56 +0000

    some corrections

--- examples/fibonacci.py
+++ examples/fibonacci.py
@@ -7,42 +7,47 @@ from code.utils.mathlogic import *
 from code.utils.config import *
 
 class Fibonacci(ReNCoDeProb):
-	terms = ['inputs[0]','inputs[1]']
+        terms = ['inputs[0]','inputs[1]']
         feedback = True
-	def __init__(self, evaluate):
-		ReNCoDeProb.__init__(self,evaluate)
-		self.labels['inputs[0]'] = '0'
-		self.labels['inputs[1]'] = '1'
+        def __init__(self, evaluate):
+                ReNCoDeProb.__init__(self,evaluate)
+                self.labels['inputs[0]'] = '0'
+                self.labels['inputs[1]'] = '1'
 
 
 def evaluate(ind, nelems = 10, **kwargs):
         circuit = ind.getcircuit()
-	if not any([cnodeinput < 0
-		    for c in circuit
-		    for cnodeinput in c[2]]):
-		return nelems + 1
-	ok = 0
-	resultdict = dict(zip([c[0] for c in circuit],
-			      [1]*len(circuit)))
-	last1 = 0
-	last2 = 0
-	for cur in range(nelems):
-		result = evaluatecircuit(circuit, regressionfun,
-					 resultdict, *[0,1])
-		expected = last1 + last2
-		#print result, expected
-		if cur == 0 and result == 0:
-			ok += 1
-			last1 = 1
-		elif cur==1 and result == expected:
-			ok += 1
-			last2 = 1
-		elif cur > 1:
-			if result == expected:
-				ok += 1
-			last2 = last1
-			last1 = expected
-	return  nelems - ok
+        if not any([cnodeinput < 0
+                    for c in circuit
+                    for cnodeinput in c[2]]):
+                return nelems + 1
+        ok = 0
+        resultdict = dict(zip([c[0] for c in circuit],
+                              [0]*len(circuit)))
+        last1 = 0
+        last2 = 0
+        for cur in range(nelems):
+            result = evaluatecircuit(circuit, regressionfun,
+                                     resultdict, *[0,1])
+            expected = last1 + last2
+                #print result, expected
+            if cur == 0 and result == 0:
+                ok += 1
+                    #last1 = 1
+            elif cur==1 and result == 1:
+                ok += 1
+                last1 = 1
+            elif cur > 1:
+                if result == expected:
+                        ok += 1
+                else:
+                        break
+                last2 = last1
+                last1 = expected
+            else:
+                break
+
+        return  nelems - ok
 
 if __name__ == '__main__':
     p  = Fibonacci(evaluate)
